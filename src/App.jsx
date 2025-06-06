6f840cfd5b75af0a4bec61baa93c4f7e00d06bfd
dulanjaya96 <nc.dulanjaya@gmail.com>
2025-06-06 19:43:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">
+      Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
